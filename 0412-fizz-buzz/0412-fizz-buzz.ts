37c646b4da64aa93bb7edde5b502703685f68242
usmanhaider140 <79703128+usmanhaider140@users.noreply.github.com>
2023-08-30 18:43:37 +0500

    Time: 71 ms (30.06%), Space: 44.9 MB (66.8%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.ts
+++ 0412-fizz-buzz/0412-fizz-buzz.ts
@@ -1,15 +1,22 @@
 function fizzBuzz(n: number): string[] {
     const result: string[] = []
     for(let i = 1; i <= n; i++) {
-        if( i % 3 === 0 && i % 5 === 0) {
-            result.push('FizzBuzz') 
-        } else if (i % 3 === 0) {
-            result.push('Fizz')
-        } else if (i % 5 === 0) {
-            result.push('Buzz')
-        } else {
-            result.push(String(i))
+        const divisibleByThree = i % 3 === 0;
+        const divisibleByFive = i % 5 === 0; 
+
+        let currentString = '';
+
+         if (divisibleByThree) {
+            currentString += 'Fizz'
+        } 
+        if (divisibleByFive) {
+            currentString += 'Buzz'
+        } 
+        if(currentString === '') {
+            currentString = String(i)
         }
+        
+        result.push(currentString)
     }
 
     return result;
